1b5b48574bbf88164b20281a61a42564c8cf2dc8
yannickwesley <yannickkarimou@gmail.com>
2021-02-05 18:57:37 +0000

    ajout des EJBs

---
+++ src/java/comptebancaire/gestionnaires/GestionnaireCompteBancaire.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comptebancaire.gestionnaires;
+
+import comptebancaire.modeles.CompteBancaire;
+import java.util.Collection;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author kykwy
+ */
+@Stateless
+public class GestionnaireCompteBancaire {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    public CompteBancaire creerCompteBancaire(String firstName, String lastName, String accountNumber, double balance){
+        CompteBancaire c = new CompteBancaire(firstName,lastName,accountNumber,balance);
+        em.persist(c);
+        return c;
+    }
+        public Collection<CompteBancaire> getAllBankAccount() {
+        // Exécution d'une requête équivalente à un select *
+        Query q = em.createQuery("select c from CompteBancaire c");
+        return q.getResultList();
+    }
+    
+    
+}
---
+++ src/java/comptebancaire/modeles/CompteBancaire.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comptebancaire.modeles;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author kykwy
+ */
+@Entity
+public class CompteBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String accountNumber;
+    private double balance;
+
+    public CompteBancaire() {
+    }
+
+    public CompteBancaire(String firstName, String lastName, String accountNumber, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CompteBancaire)) {
+            return false;
+        }
+        CompteBancaire other = (CompteBancaire) object;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "comptebancaire.modeles.CompteBancaire[ id=" + id + " ]";
+    }
+    
+}
